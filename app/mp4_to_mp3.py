26a33f00a781b1764f8fff2f85eee52305a28ab5
illia lavrivsky <ilia280703@gmail.com>
2024-06-28 18:34:36 +0300

    convertation function +bitrate

--- app/mp4_to_mp3.py
+++ app/mp4_to_mp3.py
@@ -9,7 +9,12 @@ if not os.path.exists("mp4_files"):
 if not os.path.exists("mp3_files"):
     os.makedirs("mp3_files")
 
-#Video to audio convertation
-video = VideoFileClip("mp4_files/yt1s.com -  Devil May Cry 5  I AM THE STORM THAT IS APPROACHING BUT IN 4K_1080pFHR.mp4")
-video.audio.write_audiofile("mp3_files/BuryTheLight.mp3")
-video.close()
+test_path = "yt1s.com -  Devil May Cry 5  I AM THE STORM THAT IS APPROACHING BUT IN 4K_1080pFHR.mp4"
+#Video to audio convertation function
+def m4_convertation_mp3(file_name, bitrate):
+    video = VideoFileClip(f"mp4_files/{file_name}")
+    file_path_mp3 = file_name[:-len(".mp4")]+".mp3"
+    video.audio.write_audiofile(f"mp3_files/{file_path_mp3}", bitrate=bitrate)
+    video.close()
+
+m4_convertation_mp3(test_path, "10k")
