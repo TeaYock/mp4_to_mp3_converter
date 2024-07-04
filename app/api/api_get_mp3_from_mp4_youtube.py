fb464ce3d675db08f84a22057267de56d54940a1
illia lavrivsky <ilia280703@gmail.com>
2024-07-04 13:22:53 +0300

    make youtube convertation to mp3, delete dir making bug

--- app/api/api_get_mp3_from_mp4_youtube.py
+++ app/api/api_get_mp3_from_mp4_youtube.py
@@ -1,9 +1,19 @@
-from flask import Flask, send_file
+from flask import Flask, request, send_file
+from app.mp4_to_mp3 import m4_convertation_mp3, youtube_convertation_mp3
 app = Flask(__name__)
 
+"""
 @app.route('/m4_convertation_mp3')
-def download_file():
+def m4_convertation_mp3_api():
     file_path="../mp3_files/[ Devil May Cry 5 ] I AM THE STORM THAT IS APPROACHING BUT IN 4K.mp3"
+    return send_file(file_path, as_attachment=True)
+"""
+
+#youtube url to mp3 convertation with download on client side
+@app.route('/youtube_convertation_mp3', methods=['GET'])
+def youtube_convertation_mp3_api():
+    youtube_url = request.args.get('url', '')
+    file_path=youtube_convertation_mp3(youtube_url)
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
--- app/mp4_to_mp3.py
+++ app/mp4_to_mp3.py
@@ -3,33 +3,36 @@ from pytube import YouTube
 import os
 
 #Creating directories for mp4 files
-if not os.path.exists("mp4_files"):
-    os.makedirs("mp4_files")
+def crating_m4_dir():
+    if not os.path.exists("mp4_files"):
+        os.makedirs("mp4_files")
 
 #Creating directories for mp3 files
-if not os.path.exists("mp3_files"):
-    os.makedirs("mp3_files")
+def creating_mp3_dir():
+    if not os.path.exists("mp3_files"):
+        os.makedirs("mp3_files")
 
 #test video
-test_path = "yt1s.com -  Devil May Cry 5  I AM THE STORM THAT IS APPROACHING BUT IN 4K_1080pFHR.mp4"
+#test_path = "yt1s.com -  Devil May Cry 5  I AM THE STORM THAT IS APPROACHING BUT IN 4K_1080pFHR.mp4"
 
 #Video to audio convertation function
 def m4_convertation_mp3(file_name, bitrate):
-    video = VideoFileClip(f"mp4_files/{file_name}")
+    video = VideoFileClip(f"../mp4_files/{file_name}")
     file_path_mp3 = file_name[:-len(".mp4")]+".mp3"
     video.audio.write_audiofile(f"mp3_files/{file_path_mp3}", bitrate=bitrate)
     video.close()
 
 #test url
-DMC_url="https://www.youtube.com/watch?v=d-ggzGbsEWE"
+#DMC_url="https://www.youtube.com/watch?v=d-ggzGbsEWE"
 
 #YouTube url to audio convertation function
-def youtube_convertation_mp3(youtube_url):
+def youtube_convertation_mp3(youtube_url: str) -> str:
     video = YouTube(youtube_url)
     audio_stream = video.streams.filter(only_audio=True).first()
-    audio_file_path = audio_stream.download(filename=f"mp3_files/{video.title}.mp3")
+    audio_file_path = audio_stream.download(filename=f"../mp3_files/{video.title}.mp3")
+    return audio_file_path
 
 
 #Test useges
-m4_convertation_mp3(test_path, "10k")
-youtube_convertation_mp3(DMC_url)
+#m4_convertation_mp3(test_path, "10k")
+#youtube_convertation_mp3(DMC_url)
